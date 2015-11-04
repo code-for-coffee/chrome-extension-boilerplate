78813e79110a8ba63212d1f5a21beb84f0f175c1
James <jtraverjr@gmail.com>
2015-11-03 21:43:29 -0600

    added username storage

--- popup.js
+++ popup.js
@@ -14,6 +14,27 @@ chromeApp.topics = [
   'Promotional Partner',
   'Classes & Workshops Instructor',
 ];
+chromeApp.usernameCheck = function() {
+  chrome.storage.sync.get('username', function(value) {
+    if (!value) {
+      console.log('No username set. We\'ll fix that');
+      chromeApp.saveUsername();
+    }
+    console.log('username exists. we are all set.');
+  });
+}
+chromeApp.saveUsername = function() {
+  var username = prompt("Hi! GA Chicago Contacts extension here. This will only appear once*. What is your name?");
+  if (!username) {
+    alert('Error: No username entered.');
+    chromeApp.saveUsername();
+  }
+  // Save it using the Chrome extension storage API.
+  chrome.storage.sync.set({'username': username}, function() {
+    // Notify that we saved.
+    alert('Username saved.');
+  });
+}
 chromeApp.renderDropdown = function(id, querySelectorContainer, listOfDropdownItems) {
   var dropdownObj = document.createElement('select');
   dropdownObj.id = id;
@@ -29,6 +50,8 @@ chromeApp.renderDropdown = function(id, querySelectorContainer, listOfDropdownIt
 
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    chromeApp.saveUsername('bob');
 
     chromeApp.renderDropdown('contact-reason', document.getElementById('wrapper'), chromeApp.topics);
 
