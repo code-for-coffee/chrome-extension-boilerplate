181a11ce5e2c7ce84a4a9beeee32ad6fef198be7
James <jtraverjr@gmail.com>
2015-11-03 21:32:48 -0600

    dropdown list

--- popup.js
+++ popup.js
@@ -2,8 +2,34 @@
  * Created by codeforcoffee on 11/3/2015.
  */
 
+var chromeApp = {} || new Object;
+chromeApp.topics = [
+  'Meet & Hire List',
+  'Actively Hiring',
+  'Interested in private hiring event',
+  'Contribute to Outcomes Programming',
+  'Employee Benefits Program',
+  'Event Partner',
+  'Community Benefits Program',
+  'Promotional Partner',
+  'Classes & Workshops Instructor',
+];
+chromeApp.renderDropdown = function(id, querySelectorContainer, listOfDropdownItems) {
+  var dropdownObj = document.createElement('select');
+  dropdownObj.id = id;
+  for (var i = 0; i < listOfDropdownItems.length; i++) {
+    var option = document.createElement("option");
+    option.value = listOfDropdownItems[i];
+    option.text = listOfDropdownItems[i];
+    dropdownObj.appendChild(option);
+  }
+  querySelectorContainer.appendChild(dropdownObj);
+  return true;
+};
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
-    // code to execute when the popup.html has loaded
+    chromeApp.renderDropdown('contact-reason', document.getElementById('wrapper'), chromeApp.topics);
 
 });
